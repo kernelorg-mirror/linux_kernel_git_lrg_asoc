c0a79ef96210592270bd380ba0e77990f5918500
Andi Kleen <ak@linux.intel.com>
2012-05-10 17:25:30 -0700

    perf, x86: Support PERF_SAMPLE_ADDR v2
    Haswell supplies the address for every PEBS memory event, so always fill it in
    when the user requested it.  It will be 0 when not useful (no memory access)
    On Nehalem it was supported for some load events only, on Sandy-bridge
    also for some stores.
    
    The user code already supports reporting this, since that was added
    for PPC some time ago.
    
    v2: Now include fmt1 too, so it works on Nehalem and later.
    Signed-off-by: Andi Kleen <ak@linux.intel.com>

--- arch/x86/kernel/cpu/perf_event_intel_ds.c
+++ arch/x86/kernel/cpu/perf_event_intel_ds.c
@@ -646,6 +646,10 @@ static void __intel_pmu_pebs_event(struct perf_event *event,
 	else
 		regs.flags &= ~PERF_EFLAGS_EXACT;
 
+	if ((event->attr.sample_type & PERF_SAMPLE_ADDR) &&
+		x86_pmu.intel_cap.pebs_format >= 1)
+		data.addr = pebs_hsw->nhm.dla;
+
 	if (has_branch_stack(event))
 		data.br_stack = &cpuc->lbr_stack;
 
