3589bb5107c644e6a4f58aad0168523fd3cb415a
Andi Kleen <ak@linux.intel.com>
2012-09-19 16:42:22 -0700

    perf, x86: Add Haswell specific transaction flag reporting
    In the PEBS handler report the transaction flags using the new
    generic transaction flags facility. Most of them come from
    the "tsx_tuning" field in PEBSv2, but the abort code is derived
    from the RAX register reported in the PEBS record.
    
    Signed-off-by: Andi Kleen <ak@linux.intel.com>

--- arch/x86/kernel/cpu/perf_event_intel_ds.c
+++ arch/x86/kernel/cpu/perf_event_intel_ds.c
@@ -692,6 +692,14 @@ static void __intel_pmu_pebs_event(struct perf_event *event,
 			data.weight = pebs_hsw->nhm.lat;
 	}
 
+	if ((event->attr.sample_type & PERF_SAMPLE_TRANSACTION) &&
+	    x86_pmu.intel_cap.pebs_format >= 2) {
+		data.transaction = pebs_hsw->tsx_tuning >> 32;
+		if ((data.transaction & PERF_SAMPLE_TXN_TRANSACTION) &&
+		    (pebs->ax & 1))
+			data.transaction |= pebs->ax & 0xff000000;
+	}
+
 	if (has_branch_stack(event))
 		data.br_stack = &cpuc->lbr_stack;
 
