555a05baa39da1017bb6e9f75c61bc6f5fb5b8fa
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2013-01-11 13:32:48 +0200

    dw_dmac: don't wait for FIFO_EMPTY endlessly in dwc_chan_pause
    When we pause the channel after transfer is finished we will stuck in the
    dwc_chan_pause(), because the FIFO_EMPTY flag will never be asserted. To avoid
    the endless loop we introduce a timeout here (*). The proper solution is to
    calculate the residue and avoid busyloop when transfer is done, however it's
    not simple and fast.
    
    Unfortunately we can't use cpu_relax() in conjunction with jiffies checker, due
    to we have interrupts disabled by spin_lock_irqsave() and there is a chance
    that no interrupts will come to update the jiffies..
    
    (*) The 100us timeout might be considered as a big one, however we enter to that
        state only when we have the transfer is already finished.
    
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>

--- drivers/dma/dw_dmac.c
+++ drivers/dma/dw_dmac.c
@@ -1022,10 +1022,11 @@ set_runtime_config(struct dma_chan *chan, struct dma_slave_config *sconfig)
 static inline void dwc_chan_pause(struct dw_dma_chan *dwc)
 {
 	u32 cfglo = channel_readl(dwc, CFG_LO);
+	unsigned int count = 100;	/* 100us timeout */
 
 	channel_writel(dwc, CFG_LO, cfglo | DWC_CFGL_CH_SUSP);
-	while (!(channel_readl(dwc, CFG_LO) & DWC_CFGL_FIFO_EMPTY))
-		cpu_relax();
+	while (!(channel_readl(dwc, CFG_LO) & DWC_CFGL_FIFO_EMPTY) && --count)
+		udelay(1);
 
 	dwc->paused = true;
 }
