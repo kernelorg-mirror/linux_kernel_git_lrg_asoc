kernelorg-mirror/linux_kernel_git_ak_linux-misc

perf, x86: Suppress duplicated abort LBR records
Haswell always give an extra LBR record after every TSX abort. This can confuse
some clients. Suppress the extra record.

This only works when the abort is visible in the window, that is if the
extra record is the last entry in the LBR. If the abort has already
left it it will stay.

Signed-off-by: Andi Kleen <ak@linux.intel.com>

arch/x86/kernel/cpu/perf_event.h
@@ -404,6 +404,7 @@ struct x86_pmu {
 	int		lbr_nr;			   /* hardware stack size */
 	u64		lbr_sel_mask;		   /* LBR_SELECT valid bits */
 	const int	*lbr_sel_map;		   /* lbr_select mappings */
+	bool		lbr_double_abort;	   /* duplicated abort records */
 
 	/*
 	 * Extra registers for events

kernelorg-mirror/linux_kernel_git_lrg_asoc

arch/x86/kernel/cpu/perf_event.h
@@ -399,6 +399,7 @@ struct x86_pmu {
 	int		lbr_nr;			   /* hardware stack size */
 	u64		lbr_sel_mask;		   /* LBR_SELECT valid bits */
 	const int	*lbr_sel_map;		   /* lbr_select mappings */
+	bool		lbr_double_abort;	   /* duplicated lbr aborts */
 
 	/*
 	 * Extra registers for events