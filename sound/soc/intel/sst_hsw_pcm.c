dc31f7f4a65c3b9d8ff699ef7239423d98b34c0d
Liam Girdwood <liam.r.girdwood@linux.intel.com>
2013-10-22 13:56:34 +0100

    ASoC: HSW PCM: Use hard coded values for module offsets.
    TODO: how do we extract this from the FW ?
    
    Signed-off-by: Liam Girdwood <liam.r.girdwood@linux.intel.com>

--- sound/soc/intel/sst_hsw_pcm.c
+++ sound/soc/intel/sst_hsw_pcm.c
@@ -485,30 +485,44 @@ static int hsw_pcm_hw_params(struct snd_pcm_substream *substream,
 		return ret;
 	}
 
-	/* TODO: validate module parameters for allocate stream */
-
-	/* TODO: validate - persistant data */
 	dsp = sst_hsw_get_dsp(hsw);
+	/* TODO: validate - persistant data */
 	module_data = sst_module_get_config(dsp, 0, SST_DATA_P);
 	if (module_data == NULL) {
 		dev_err(rtd->dev, "PCM: failed to get module P config\n");
 		return -EINVAL;
 	}
-	sst_hsw_stream_set_module_info(hsw, pcm_data->stream,
-		SST_HSW_MODULE_BASE_FW, module_data->entry);
 
-	sst_hsw_stream_set_pmemory_info(hsw, pcm_data->stream,
-		module_data->offset, module_data->size);
+	/* TODO: validate module parameters for allocate stream */
+	if (stream_type == SST_HSW_STREAM_TYPE_CAPTURE)
+	{
+		sst_hsw_stream_set_module_info(hsw, pcm_data->stream,
+			SST_HSW_MODULE_PCM_CAPTURE, module_data->entry);
+		sst_hsw_stream_set_pmemory_info(hsw, pcm_data->stream,
+			0x449400, 0x4000);
+		sst_hsw_stream_set_smemory_info(hsw, pcm_data->stream,
+			0x400000, 0);
+	} else { /* stream_type == SST_HSW_STREAM_TYPE_SYSTEM */
+		sst_hsw_stream_set_module_info(hsw, pcm_data->stream,
+			SST_HSW_MODULE_PCM_SYSTEM, module_data->entry);
 
-	/* TODO: validate - scratch data */
-	module_data = sst_module_get_config(dsp, 0, SST_DATA_S);
-	if (module_data == NULL) {
-		dev_err(rtd->dev, "PCM: failed to get module S config\n");
-		return -EINVAL;
+		sst_hsw_stream_set_pmemory_info(hsw, pcm_data->stream,
+			module_data->offset, module_data->size);
+		sst_hsw_stream_set_pmemory_info(hsw, pcm_data->stream,
+			0x44d400, 0x3800);
+
+		/* TODO: validate - scratch data */
+		module_data = sst_module_get_config(dsp, 0, SST_DATA_S);
+		if (module_data == NULL) {
+			dev_err(rtd->dev, "PCM: failed to get module S config\n");
+			return -EINVAL;
+		}
+
+		sst_hsw_stream_set_smemory_info(hsw, pcm_data->stream,
+			module_data->offset, module_data->size);
+		sst_hsw_stream_set_smemory_info(hsw, pcm_data->stream,
+			0x400000, 0);
 	}
-
-	sst_hsw_stream_set_smemory_info(hsw, pcm_data->stream,
-		module_data->offset, module_data->size);
 
 	ret = sst_hsw_stream_commit(hsw, pcm_data->stream);
 	if (ret < 0) {
