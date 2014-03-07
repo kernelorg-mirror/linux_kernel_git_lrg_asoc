9ceeed08de351935b602fc353afc32db705525af
Liam Girdwood <liam.r.girdwood@linux.intel.com>
2014-03-07 17:45:19 +0000

    ASoC: Intel: Dont do any reset, reboot or stalling during PM
    Test patch for "always on" mode.
    
    Signed-off-by: Liam Girdwood <liam.r.girdwood@linux.intel.com>

--- sound/soc/intel/sst-baytrail-pcm.c
+++ sound/soc/intel/sst-baytrail-pcm.c
@@ -457,6 +457,7 @@ static int sst_byt_pcm_dev_remove(struct platform_device *pdev)
 }
 
 #ifdef CONFIG_PM
+#if 0
 static int sst_byt_pcm_dev_suspend_noirq(struct device *dev)
 {
 	struct sst_pdata *sst_pdata = dev_get_platdata(dev);
@@ -520,6 +521,7 @@ static const struct dev_pm_ops sst_byt_pm_ops = {
 #define SST_BYT_PM_OPS	(&sst_byt_pm_ops)
 #else
 #define SST_BYT_PM_OPS	NULL
+#endif
 #endif
 
 static struct platform_driver sst_byt_pcm_driver = {
