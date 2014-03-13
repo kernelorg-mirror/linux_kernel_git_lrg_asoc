f4e56013fe2dcffcb2e33b13993b1e9ed836b209
Liam Girdwood <liam.r.girdwood@linux.intel.com>
2014-03-13 16:10:56 +0000

    ASoC: mxa98090: Test - Add delay at resume for MCLK
    MCLK comes up about 870ms after DVDD. Add a test to wait that long prior
    to register IO. Is this really needed ??
    
    Signed-off-by: Liam Girdwood <liam.r.girdwood@linux.intel.com>

--- sound/soc/codecs/max98090.c
+++ sound/soc/codecs/max98090.c
@@ -2421,6 +2421,9 @@ static int max98090_resume(struct device *dev)
 	struct max98090_priv *max98090 = dev_get_drvdata(dev);
 	unsigned int status;
 
+	/* TODO: Is this wait really needed */
+	msleep(870);
+
 	regcache_mark_dirty(max98090->regmap);
 
 	max98090_reset(max98090);
