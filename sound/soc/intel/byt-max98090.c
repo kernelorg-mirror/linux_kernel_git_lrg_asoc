2dc6afe0540a52cfb6bd1590172891865a0a0957
Jarkko Nikula <jarkko.nikula@linux.intel.com>
2014-03-13 16:56:21 +0200

    ASoC: Intel: Allow Baytrail machine drivers and SST core go to suspend
    Since there is no support for compressed audio in Baytrail ADSP firmware
    there is no need to leave it on either since ALSA PCM buffers are too small
    for leaving ADSP on for playing or recording during the suspend.
    
    Implement PM callbacks to Baytrail byt-max98090.c and byt-rt5640.c
    machine drivers that call snd_soc_suspend and snd_soc_resume functions
    and unset the ignore_suspend fields in DAI links.
    
    This makes soc-core and ALSA core gracefully to suspend and resume
    stream and call sst_byt_pcm_trigger() during suspend-resume cycle.
    
    Signed-off-by: Jarkko Nikula <jarkko.nikula@linux.intel.com>

--- sound/soc/intel/byt-max98090.c
+++ sound/soc/intel/byt-max98090.c
@@ -290,7 +290,6 @@ static struct snd_soc_dai_link byt_dailink[] = {
 		.codec_name = "i2c-193C9890:00",
 		.platform_name = "baytrail-pcm-audio",
 		.init = byt_init,
-		.ignore_suspend = 1,
 		.ops = &byt_aif1_ops,
 		.dai_fmt = SND_SOC_DAIFMT_I2S | SND_SOC_DAIFMT_NB_NF |
 			SND_SOC_DAIFMT_CBS_CFS,
@@ -303,7 +302,6 @@ static struct snd_soc_dai_link byt_dailink[] = {
 		.codec_name = "i2c-193C9890:00",
 		.platform_name = "baytrail-pcm-audio",
 		.init = NULL,
-		.ignore_suspend = 1,
 		.ops = &byt_aif1_ops,
 		.dai_fmt = SND_SOC_DAIFMT_I2S | SND_SOC_DAIFMT_NB_NF |
 			SND_SOC_DAIFMT_CBS_CFS,
@@ -321,6 +319,17 @@ static struct snd_soc_card snd_soc_card_byt = {
 	.controls = byt_mc_controls,
 	.num_controls = ARRAY_SIZE(byt_mc_controls),
 };
+
+#ifdef CONFIG_PM_SLEEP
+static const struct dev_pm_ops byt_max98090_pm_ops = {
+	.suspend = snd_soc_suspend,
+	.resume = snd_soc_resume,
+};
+
+#define BYT_MAX98090_PM_OPS	(&byt_max98090_pm_ops)
+#else
+#define BYT_MAX98090_PM_OPS	NULL
+#endif
 
 static int snd_byt_mc_probe(struct platform_device *pdev)
 {
@@ -369,6 +378,7 @@ static struct platform_driver snd_byt_mc_driver = {
 	.driver = {
 		.name = "byt-max98090",
 		.owner = THIS_MODULE,
+		.pm = BYT_MAX98090_PM_OPS,
 	},
 };
 module_platform_driver(snd_byt_mc_driver)
