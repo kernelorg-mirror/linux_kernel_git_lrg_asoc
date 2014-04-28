c4967dccd52d7c4f1b503b1c29fbc6ecbdd1a5f5
Liam Girdwood <liam.r.girdwood@linux.intel.com>
2014-04-28 11:34:01 +0100

    ASoC: Intel: Add widgets for Broadwell Speaker, Mic and Jack
    Signed-off-by: Liam Girdwood <liam.r.girdwood@linux.intel.com>

--- sound/soc/intel/broadwell.c
+++ sound/soc/intel/broadwell.c
@@ -28,7 +28,11 @@
 
 static const struct snd_soc_dapm_widget broadwell_widgets[] = {
 	SND_SOC_DAPM_HP("Headphones", NULL),
-	SND_SOC_DAPM_MIC("Mic", NULL),
+	SND_SOC_DAPM_SPK("Speaker", NULL),
+	SND_SOC_DAPM_MIC("Mic Jack", NULL),
+	SND_SOC_DAPM_MIC("DMIC1", NULL),
+	SND_SOC_DAPM_MIC("DMIC2", NULL),
+	SND_SOC_DAPM_LINE("Line Jack", NULL),
 };
 
 static const struct snd_soc_dapm_route broadwell_rt286_map[] = {
