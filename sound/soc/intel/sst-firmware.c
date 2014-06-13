986b48ca19c8cf2543e83996f6d04c80f3089812
Liam Girdwood <liam.r.girdwood@linux.intel.com>
2014-06-13 17:40:35 +0100

    ASoC: Intel: Add fw object when allocating scratch blocks
    Add the FW object that is allocating the scratch block so that we can
    remove it when the associated FW object is removed.
    
    Signed-off-by: Liam Girdwood <liam.r.girdwood@linux.intel.com>

--- sound/soc/intel/sst-firmware.c
+++ sound/soc/intel/sst-firmware.c
@@ -767,7 +767,8 @@ void sst_mem_block_unregister_all(struct sst_dsp *dsp)
 EXPORT_SYMBOL_GPL(sst_mem_block_unregister_all);
 
 /* allocate scratch buffer blocks */
-struct sst_module *sst_mem_block_alloc_scratch(struct sst_dsp *dsp)
+struct sst_module *sst_mem_block_alloc_scratch(struct sst_dsp *dsp,
+	struct sst_fw *sst_fw)
 {
 	struct sst_module *sst_module, *scratch;
 	struct sst_mem_block *block, *tmp;
@@ -815,6 +816,8 @@ struct sst_module *sst_mem_block_alloc_scratch(struct sst_dsp *dsp)
 	/* assign the same offset of scratch to each module */
 	list_for_each_entry(sst_module, &dsp->module_list, list)
 		sst_module->s.offset = scratch->s.offset;
+	list_add(&scratch->list, &dsp->module_list);
+	scratch->sst_fw = sst_fw;
 
 	mutex_unlock(&dsp->mutex);
 	return scratch;
