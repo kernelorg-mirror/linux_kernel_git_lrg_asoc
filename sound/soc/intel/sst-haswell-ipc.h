425e644bc31857efcd85733ef4701ed8835deb8a
Liam Girdwood <liam.r.girdwood@linux.intel.com>
2014-08-29 17:44:07 +0100

    ASoC: Intel: Add TDM support to HSW/BDW SSP port
    Add TDM support to SSP port via DSP IPC SetDeviceFormat message.
    
    Signed-off-by: Liam Girdwood <liam.r.girdwood@linux.intel.com>

--- sound/soc/intel/sst-haswell-ipc.h
+++ sound/soc/intel/sst-haswell-ipc.h
@@ -86,6 +86,7 @@ enum sst_hsw_device_mclk {
 enum sst_hsw_device_mode {
 	SST_HSW_DEVICE_CLOCK_SLAVE   = 0,
 	SST_HSW_DEVICE_CLOCK_MASTER  = 1,
+	SST_HSW_DEVICE_TDM_CLOCK_MASTER = 2,
 };
 
 /* DX Power State */
@@ -297,7 +298,8 @@ struct sst_hsw_ipc_device_config_req {
 	u32 clock_frequency;
 	u32 mode;
 	u16 clock_divider;
-	u16 reserved;
+	u8 channels;
+	u8 reserved;
 } __attribute__((packed));
 
 /* Audio Data formats */
