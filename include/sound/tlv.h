a11709ade95d1ae31339f3a8333a916f01116e7b
Liam Girdwood <liam.r.girdwood@linux.intel.com>
2015-04-09 18:11:17 +0100

    ALSA: topology: Export ID types for TLV controls.
    Make sure userspace can define TLV controls for topolofy using the correct
    type numbers.
    
    Signed-off-by: Liam Girdwood <liam.r.girdwood@linux.intel.com>

--- include/sound/tlv.h
+++ include/sound/tlv.h
@@ -31,12 +31,7 @@
  *                       ~(sizeof(unsigned int) - 1)) ....
  */
 
-#define SNDRV_CTL_TLVT_CONTAINER 0	/* one level down - group of TLVs */
-#define SNDRV_CTL_TLVT_DB_SCALE	1       /* dB scale */
-#define SNDRV_CTL_TLVT_DB_LINEAR 2	/* linear volume */
-#define SNDRV_CTL_TLVT_DB_RANGE 3	/* dB range container */
-#define SNDRV_CTL_TLVT_DB_MINMAX 4	/* dB scale with min/max */
-#define SNDRV_CTL_TLVT_DB_MINMAX_MUTE 5	/* dB scale with min/max with mute */
+#include <uapi/sound/tlv.h>
 
 #define TLV_ITEM(type, ...) \
 	(type), TLV_LENGTH(__VA_ARGS__), __VA_ARGS__
