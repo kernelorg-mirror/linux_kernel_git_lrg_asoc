e0f3ff6445cce54db4044697c64b524de6ad51a3
Liam Girdwood <liam.r.girdwood@linux.intel.com>
2017-07-13 21:29:42 +0100

    ASoC: core: topology FE support part 2. To be squashed.
    Signed-off-by: Liam Girdwood <liam.r.girdwood@linux.intel.com>

--- sound/soc/soc-core.c
+++ sound/soc/soc-core.c
@@ -2077,6 +2077,7 @@ static void soc_check_tplg_fes(struct snd_soc_card *card)
 			}
 
 			dai_link->platform_name = platform->component.name;
+			dai_link->cpu_dai_name = platform->component.name;
 		}
 
 	}
